d5e6e3d9911681107f9a150e2323d8a5ec9afddd
Mathias Piletti <mathiaspiletti@Mathiass-MacBook-Pro.local>
2018-11-03 14:23:05 +0100

    React package

--- src/index.js
+++ src/index.js
@@ -2,38 +2,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// REDUX
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import { createLogger } from 'redux-logger';
-
 // MODULES
 import App from './js/app'
 
 // SCSS STYLES
 import './style/style.scss';
 
-// REDUCERS
-import reducer_notes from './js/reducers/reducer_notes';
-import reducer_profile from './js/reducers/reducer_profile';
-
-// REDUX STORE
-const store = createStore(
-    combineReducers({
-        notes: reducer_notes,
-        profile: reducer_profile
-    }),
-    applyMiddleware(
-        createLogger()
-    )
-)
-
 // HMR
 if (module.hot) { module.hot.accept() }
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App/>
-    </Provider>,
+    <App/>,
     document.getElementById('root')
 );
--- src/js/app.js
+++ src/js/app.js
@@ -1,25 +1,13 @@
 import React from 'react';
-import ComponentTodo from './components/component_todo'
-import ComponentControl from './components/component_control'
-
-import mapDispatchToProps from './actions/actions_dispatcher'
-
-import { connect } from 'react-redux';
 
 class App extends React.Component {
     render() {
         return (
             <div>
-                <h1> Matis - React & Redux starterpack </h1>
-                <ComponentTodo setName={this.props.actions.profile.setName} />
-                <ComponentControl />
+                <h1> Matis - React starterpack </h1>
             </div>
         );
     }
 }
 
-function mapStateToProps(store) {
-    return { store }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(App)
+export default App
